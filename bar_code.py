bdfe2e8dc5d3458579bedc89746a86b89f3b6798
Mashael2 <malabbad1@mail.weber.edu>
2017-03-28 12:57:05 -0600

    added Task2 files

---
+++ bar_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+
+
+#Main function
+def main():
+    """
+    Test Function
+    """
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
