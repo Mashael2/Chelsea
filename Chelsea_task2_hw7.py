963158b1aec056d915dc5023fe5068dd467add69
Mashael2 <malabbad1@mail.weber.edu>
2017-03-31 11:20:24 -0600

    Updated Task2 module3

--- Chelsea_task2_hw7.py
+++ Chelsea_task2_hw7.py
@@ -7,6 +7,19 @@ def printDigit(d):
     """
     A function to print the digits
     """
+    value = {
+            0: "||:::",
+            1: ":::||",
+            2: "::|:|",
+            3: "::||:",
+            4: ":|::|",
+            5: ":|:|:",
+            6: ":||::",
+            7: "|:::|",
+            8: "|::|:",
+            9: "|:|::",
+            }
+    return value.get(d, "Zip code is not all numeric")
 
 
 
@@ -15,6 +28,19 @@ def printBarCode(zipCode):
     A function to print the bar code
     """
 
+    barcode = ["|",]
+    digits = [int(i) for i in str(zipCode)]
+
+    for d in digits:
+        barcode.append(printDigit(d))
+
+    CheckDigit = sum(digits) % 10
+    if checkDigit % 10 != 5:
+        print("Zip code is not 5 digits")
+    else:
+        checkDigit = printDigit(checkDigit)
+        
+
 
 
 # Main function
@@ -22,6 +48,9 @@ def main():
     """
     Test Function
     """
+    userInput = input("Enter a zipcode: ")
+    printBarCode(userInput)
+
 
 
 if __name__ == "__main__":
