71abc543c5ab344c471ada2e3614deddaa00f186
Mashael2 <malabbad1@mail.weber.edu>
2017-03-31 11:28:05 -0600

    Added test script for Task2

---
+++ Chelsea_task2_test_hw7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+
+# Main function
+def main():
+    """
+    Test Function
+    """
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
